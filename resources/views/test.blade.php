07972e9acda09efb0a366bbee2ee4a6dda34e4ca
Evgeny <evgeny@Mac-mini-Evgeny.local>
2023-08-21 07:34:00 +0900

    20.08.23

---
+++ resources/views/test.blade.php
@@ -0,0 +1,2 @@
+<h1>Test View</h1>
+<p>{{ $var_test }}</p>
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,61 @@ Route::get('/', function () {
 Route::get('/contact', function(){
     return view('contact');
 });
+/*
 Route::post('/email', function(){
-    return '<h1>Email Page</h1>';
     if(!empty($_POST)){
         dump($_POST);
     }
+    return '<h1>Email Page</h1>';
 });
+*/
+Route::match(['post', 'get', 'PUT'], '/email', function(){
+    if(!empty($_POST)){
+        dump($_POST);
+    }
+    return '<h1>Email Page Match</h1>';
+})->name('email');
+
+Route::view('test', 'test', ['var_test'=>'Test Page']);
+
+// Route::redirect('test', 'contact', 302);
+// Route::permanentRedirect('test', 'contact');
+
+// Route::get('post/{id}', function($id){
+//     return 'Post $id';
+// });
+
+// Route::get('post/{id}/{slug}', function ($id, $slug) {
+//     return "Post $id / $slug";
+// })->where(['id' => '[0-9]+', 'slug' => '[A-Za-z0-9-]+']);
+
+// Route::get('post/{id}/{slug?}', function($id, $slug = null){
+//     return "Post $id | $slug";
+// });
+
+// Route::get('posts', function () {
+//     return 'Posts List';
+// });
+// Route::get('post/create', function () {
+//     return 'Posts Create';
+// });
+// Route::get('post/{id}/edit', function ($id) {
+//     return "Posts Edit $id";
+// });
+
+Route::prefix('admin')->group(function(){
+    Route::get('posts', function () {
+        return 'Posts List';
+    });
+    Route::get('post/create', function () {
+        return 'Posts Create';
+    });
+    Route::get('post/{id}/edit', function ($id) {
+        return "Posts Edit $id";
+    });
+});
+
+
+
+
+
