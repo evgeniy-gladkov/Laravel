b36941fd51cc6db1b47d1db33e7bdc5754a4b8b5
evkakiy <evgeniy.gladkov33@gmail.com>
2023-08-23 12:35:06 +0900

    config laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,14 @@ class HomeController extends Controller
 {
     //
     public function index(){
-        return view('home', ['name'=>'john', 'age'=>23]);
+        // return view('home', ['name'=>'john', 'age'=>23])."<br>";
+        dump($_ENV['APP_DEBUG']);
+        // dump(env('APP_DEBUG'));
+        // dump(env('MY_SETING', 'test'));
+        // dump(config('app.timezone'));
+        // dump(config('database.connections.mysql.database'));
+        
+        dump($_ENV);
     }
 
     public function test(){
